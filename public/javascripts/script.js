d2612c1fdcf786fd68dededc4dd88bb7ed08f14d
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-05-24 22:38:15 -0400

    Changed the button design and added an alert for invalid email

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     var email = $("#emailInput").val();
     var pattern = new RegExp("^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
     if (pattern.test(email) === false) {
-      alert('');
+      alert('Email not Valid!');
     } else {
         var url = "/getEmailID/" + email;
         $.post(url, function(data){
