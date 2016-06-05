8b52b9a78e0b382a782e1e0c56de4f05ed6c0226
faizanv <faizan.virani.44@gmail.com>
2016-06-04 18:27:52 -0700

    Trying to optimize script loading to no avail

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,27 +1,3 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $('#phone').addClass('shake');
-    setTimeout(function(){
-      $('#phone').attr('src', 'img/phoneB.png');
-      setTimeout(function(){
-        $('#phone').removeClass('shake');
-      }, 750);
-    }, 100);
-  }, 5000);
-});
-
-// $('form').submit(function(){
-//   var email = $("#emailInput").val();
-//   var pattern = new RegExp("^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
-//   if (pattern.test(email) === false) {
-//     alert('Email not Valid!');
-//   } else {
-//       var url = "/getEmailID/" + email;
-//       $.post(url, function(data){
-//       });
-//   }
-// });
-
 var app = angular.module('teaser', []);
 app.controller("TeaserController", ['$scope', '$http', function($scope, $http) {
   $scope.email = '';
