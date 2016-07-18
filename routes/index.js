154efb6265c33e718abf8ccb5a04dff008e48c61
Andrew Dai <theman@andrewdai.co>
2016-07-17 21:49:45 -0400

    fix indentation

--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,9 @@ var con = mysql.createConnection(mysqlConfig);
 
 
 /**
- *Code to make the connection to the database
- *
- **/
+*Code to make the connection to the database
+*
+**/
 con.connect(function(err){
   if(err){
     console.log('Error connecting to Db');
@@ -33,7 +33,7 @@ con.connect(function(err){
 });
 
 setInterval(function () {
-    con.query('SELECT 1');
+  con.query('SELECT 1');
 }, 5000);
 
 /* GET home page. */
@@ -42,37 +42,37 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/getEmailID/:email', function(req, response){
-	var newEmail = req.params.email;
-	console.log(newEmail);
-	con.query('SELECT * FROM email_list WHERE email_id = ?', newEmail, function(err, rows){
-		if (err) {
-			response.send(err);
-		} else {
-			if (rows.length === 0) {
-				var em = {email_id: newEmail};
-				con.query('INSERT INTO email_list SET ?', em, function(err, res){
-		            if (err) {
-		              response.send(err);
-		            } else {
+  var newEmail = req.params.email;
+  console.log(newEmail);
+  con.query('SELECT * FROM email_list WHERE email_id = ?', newEmail, function(err, rows){
+    if (err) {
+      response.send(err);
+    } else {
+      if (rows.length === 0) {
+        var em = {email_id: newEmail};
+        con.query('INSERT INTO email_list SET ?', em, function(err, res){
+          if (err) {
+            response.send(err);
+          } else {
 
-		            	var emailconf   = new sendgrid.Email({
-						  to      : newEmail,
-						  from    : 'hello@hackgt.com',
-						  subject : 'HackGT 2016 Subscription',
-						  text    : 'Thank you for subscribing to HackGT 2016. We will email you with any updates.'
-						});
-						sendgrid.send(emailconf, function(err, json) {
-						  if (err) { return console.error(err); }
-						  response.json({emailAdded: true});
-						});
-		            }
-
-		        });
-			} else {
-				response.send({emailAdded: false});
-			}
-		}
-	});
+            var emailconf   = new sendgrid.Email({
+              to      : newEmail,
+              from    : 'hello@hackgt.com',
+              subject : 'HackGT 2016 Subscription',
+              text    : 'Thank you for subscribing to HackGT 2016. We will email you with any updates.'
+            });
+            sendgrid.send(emailconf, function(err, json) {
+              if (err) { return console.error(err); }
+              response.json({emailAdded: true});
+            });
+          }
+          
+        });
+      } else {
+        response.send({emailAdded: false});
+      }
+    }
+  });
 });
 
 router.get('/sponsors', function(req, res, next) {
